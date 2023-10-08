305f13b9907fe1fbf3289eb9d523b8ad363f36a0
muhamadisro27 <isro.sabanur@trans-cosmos@co.id>
2023-10-08 20:05:21 +0700

    feat: drum pad machine

--- src/App.jsx
+++ src/App.jsx
@@ -1,33 +1,68 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
+// import { useState } from 'react'
+import Pad from './components/Pad';
 import './App.css'
 
 function App() {
-  const [count, setCount] = useState(0)
+  const pads = [
+    {
+      code : "q",
+      alfabet : "Q",
+      source : "https://s3.amazonaws.com/freecodecamp/drums/Heater-1.mp3",
+      name : "Heater 1",
+    },
+    {
+      code : "w",
+      alfabet : "W",
+      source : "https://s3.amazonaws.com/freecodecamp/drums/Heater-2.mp3",
+      name : "Heater 2",
+    },
+    {
+      code : "e",
+      alfabet : "E",
+      source : "https://s3.amazonaws.com/freecodecamp/drums/Heater-3.mp3",
+      name : "Heater 3",
+    },
+    {
+      code : "a",
+      alfabet : "A",
+      source : "https://s3.amazonaws.com/freecodecamp/drums/Heater-4_1.mp3",
+      name : "Heater 4",
+    },
+    {
+      code : "s",
+      alfabet : "S",
+      source : "https://s3.amazonaws.com/freecodecamp/drums/Heater-6.mp3",
+      name : "Clap",
+    },
+    {
+      code : "d",
+      alfabet : "D",
+      source : "https://s3.amazonaws.com/freecodecamp/drums/Dsc_Oh.mp3",
+      name : "Open-HH",
+    },
+    {
+      code : "z",
+      alfabet : "Z",
+      source : "https://s3.amazonaws.com/freecodecamp/drums/Kick_n_Hat.mp3",
+      name : "Kick-n'-Hat",
+    },
+    {
+      code : "x",
+      alfabet : "X",
+      source : "https://s3.amazonaws.com/freecodecamp/drums/RP4_KICK_1.mp3",
+      name : "Kick",
+    },
+    {
+      code : "c",
+      alfabet : "C",
+      source : "https://s3.amazonaws.com/freecodecamp/drums/Cev_H2.mp3",
+      name : "Closed-HH",
+    },
+  ];
 
   return (
     <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+      <Pad pads={pads} />
     </>
   )
 }
